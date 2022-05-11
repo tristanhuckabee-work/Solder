2f58d9d3e936bc58e1f9b59fc12848a45dea5b28
tristan <thuckabee.work@gmail.com>
2022-05-11 16:30:32 -0500

    Complete CREATE on Items

--- react-app/src/components/createItemForm/createItemForm.js
+++ react-app/src/components/createItemForm/createItemForm.js
@@ -14,7 +14,7 @@ const CreateItemForm = () => {
   const [name, setName] = useState();
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState();
-
+  const [pics, setPics] = useState('');
 
   const onSubmit = async (e) => {
     e.preventDefault()
@@ -24,55 +24,96 @@ const CreateItemForm = () => {
       name,
       description,
       price,
-      // pics
+      pics: pics.slice(1)
     }
 
     const newest = await dispatch(newItem(item));
     if (newest?.errors) setErrors(newest);
     history.push('/');
   }
+
   const updateName = (e) => setName(e.target.value);
   const updateDescription = (e) => setDescription(e.target.value);
   const updatePrice = (e) => setPrice(e.target.value);
+  const updateImages = async (e) => {
+    e.preventDefault();
+
+    let images = { ...e.target.files };
+    let pictures = `${pics?.slice()}`;
+    for (let i = 0; i < Object.keys(images).length; i++) {
+      const formData = new FormData()
+      formData.append('image', images[i])
+
+      const res = await fetch('/api/items/images', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await res.json();
+      pictures += `,${data['url']}`;
+    }
+    setPics(pictures);
+
+  }
 
   if (!user) return <Redirect to='/' />
 
   return (
-    <form onSubmit={onSubmit}>
-      <h2>Post Your Item</h2>
-      <div className='errors'>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <input
-          type='text'
-          name='name'
-          onChange={updateName}
-          value={name}
-          placeholder='Item Name'
-        ></input>
-      </div>
-      <div>
-        <textarea
-          name='description'
-          onChange={updateDescription}
-          value={description}
-          placeholder='Describe your Item here!'
-        ></textarea>
-      </div>
-      <div>
-        <input
-          type='text'
-          name='price'
-          onChange={updatePrice}
-          value={price}
-          placeholder='Price <<$##.##>>'
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <>
+      <form onSubmit={onSubmit}>
+        <h2>Post Your Item</h2>
+        <div className='errors'>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div>
+          <input
+            type='text'
+            name='name'
+            onChange={updateName}
+            value={name}
+            placeholder='Item Name'
+          ></input>
+        </div>
+        <div>
+          <textarea
+            name='description'
+            onChange={updateDescription}
+            value={description}
+            placeholder='Describe your Item here!'
+          ></textarea>
+        </div>
+        <div>
+          <input
+            type='text'
+            name='price'
+            onChange={updatePrice}
+            value={price}
+            placeholder='Price <<$##.##>>'
+          ></input>
+        </div>
+        <div>
+          <input
+            type='file'
+            accept='image/*'
+            onChange={updateImages}
+            multiple
+          />
+        </div>
+        <button type='submit'>Sign Up</button>
+      </form>
+      <form>
+        {pics.split(',').map(picture => {
+          if (picture === '') {
+            return null;
+          } else {
+            return (
+              <div className='item_images' key={picture} style={{ backgroundImage: `url(${picture})` }}></div>
+            )
+          }
+        })}
+      </form>
+    </>
   )
 }
 
