58747ab460a76f9edd84628f61b4cf54445c4171
tristan <thuckabee.work@gmail.com>
2022-05-13 16:48:44 -0500

    Search is Mostly working

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import ProtectedRoute from './components/auth/ProtectedRoute';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
 import MainSplash from './components/Main/Main';
 import NavBar from './components/NavBar';
 import ItemPage from './components/itemPage/ItemPage';
@@ -12,7 +10,6 @@ import CreateItemForm from './components/createItemForm/createItemForm'
 import EditItemForm from './components/editItemForm/editItemForm';
 
 import { authenticate } from './store/session';
-import { getAllItems } from './store/item';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -35,7 +32,6 @@ function App() {
       <Switch>
         <Route path='/home' exact={true}>
           <MainSplash />
-          {/* <LoginForm /> */}
         </Route>
         <Route path='/items/new' exact={true}>
           <CreateItemForm />
